57c5936f3d88551bf4dba69a5938f9cbe8dd9180
Scott Schupbach <ssschupbach@gmail.com>
2020-07-06 11:57:26 -0500

    Add outstanding balance to existing loan endpoints

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,9 +1,9 @@
 class LoansController < ApplicationController
   def index
-    render json: Loan.all
+    render json: Loan.all_with_balance
   end
 
   def show
-    render json: Loan.find(params[:id])
+    render json: Loan.find(params[:id]).to_h
   end
 end
--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,8 +1,16 @@
 class Loan < ActiveRecord::Base
   has_many :payments, dependent: :destroy
 
+  def self.all_with_balance
+    Loan.all.map { |loan| loan.to_h }
+  end
+
   def outstanding_balance
     return funded_amount if payments.empty?
     funded_amount - payments.inject(0) { |agg, payment| agg + (payment&.amount || 0) }
+  end
+
+  def to_h
+    attributes.merge({ outstanding_balance: outstanding_balance })
   end
 end
