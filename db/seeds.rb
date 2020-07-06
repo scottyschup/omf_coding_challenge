0436d5667466685ba77e990206acbcd2e64f7efb
Scott Schupbach <ssschupbach@gmail.com>
2020-07-06 06:18:18 -0500

    Remove unnecessary assignment from seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 loan = Loan.create!(funded_amount: 100.0)
-payments = Payment.create!(amount: 50.00, payment_date: Time.now, loan_id: loan.id)
+Payment.create!(amount: 50.00, payment_date: Time.now, loan_id: loan.id)
